3e6e80835c677e336943095840c5520f0169176a
ciarán Brady <ciarn Brady>
2016-12-08 14:59:54 +0000

    thursday the 8th 1js done

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
-function nameFunction(){
+
+function nameChange()
+{
 	
 	var name=prompt("What is your name");
-	document.getElementById("myName").innerHTML=Agent;
+	document.getElementById("myName").innerHTML= name;
 		
+}
+
+function quiz()
+{
+	
+	alert("Who is the leader of the cleanrs?")
+	
+	
 }
